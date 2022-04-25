71239f0920c106e9823fbf9ea174f5724d99d06e
Lane91 <95251140+Lane91@users.noreply.github.com>
2022-04-24 20:56:10 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,8 @@ summary(Mecha_lm)
 Coil<-read.csv(file = 'Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
 total_summary <- Coil %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
 lot_summary <-Coil %>% group_by(Manufacturing_Lot)%>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
+
+t.test(Coil$PSI,mu=1500)
+t.test(subset(Coil,Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+t.test(subset(Coil,Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+t.test(subset(Coil,Manufacturing_Lot=="Lot3")$PSI,mu=1500)
