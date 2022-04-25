c80ad5c58279c84b290a49a2f12dabec8340f5cc
Lane91 <95251140+Lane91@users.noreply.github.com>
2022-04-24 20:45:44 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,3 +2,7 @@ library(dplyr)
 MechaCar<-read.csv(file = 'MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
 Mecha_lm<-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar)
 summary(Mecha_lm)
+
+Coil<-read.csv(file = 'Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
+total_summary <- Coil %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_summary <-Coil %>% group_by(Manufacturing_Lot)%>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
